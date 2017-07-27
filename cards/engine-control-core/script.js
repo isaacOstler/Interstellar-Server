73d4406867aec48ab4bbe86950c73cf224c18379
Isaac Ostler <isaacostler@Isaacs-MacBook-Pro.local>
2017-07-27 17:20:58 -0600

    Created GUI for Engine Control Core

---
+++ cards/engine-control-core/script.js
@@ -0,0 +1,27 @@
+//copyright Isaac Ostler, July 27th 2017, all rights reserved ©
+
+var EngineControlCoreHasInit = false;
+
+Interstellar.addCoreWidget("Engine Control",function(){
+	//ensure we don't init twice
+	if(EngineControlCoreHasInit){
+		return;
+	}
+	EngineControlCoreHasInit = true;
+
+	//variables
+
+	//DOM references
+
+	//init calls
+
+	//Interstellar functions
+
+	//database observers
+
+	//preset observers
+
+	//functions
+
+	//event listeners
+});
