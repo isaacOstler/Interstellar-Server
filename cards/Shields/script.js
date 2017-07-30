d3e1fa4739e12ca39433d0c40faa93c9f1a9f83b
isaacOstler <isaacOstler1999@gmail.com>
2017-07-29 18:48:04 -0600

    Temp bug fix with updating three.js, reverting back to v83 until sensors can work with v86

--- cards/Shields/script.js
+++ cards/Shields/script.js
@@ -23,7 +23,7 @@ function init(){
 	$("#contentArea").append( renderer.domElement );
 
 	var row = 0;
-	var column = 64;
+	var column = 32;
 	var requiredColumnAmount = column * 2;
 	geometry = new THREE.SphereGeometry( 40, column, column);
 
@@ -33,16 +33,15 @@ function init(){
 			row++;
 		}
 		column++;
-		if(row < (64 / 3.5)){
+		if(row < (32 / 3.5)){
 			geometry.faces[i].materialIndex = 0;
-		}else if(row < (64 / 3.5) * 2.5){
+		}else if(row < (32 / 3.5) * 2.5){
 			for(var j = 0;j < 4;j++){
 				matIndex = j + 1;
 				var cap = Math.round((requiredColumnAmount / 4) * j);
 				if(isOdd(cap)){
 					cap--;
 				}
-				console.log(cap);
 				if(column > cap){
 					geometry.faces[i].materialIndex = matIndex;
 				}
@@ -73,17 +72,18 @@ function init(){
 	}
 
 	var grid = new THREE.GridHelper(100, 20);
-	scene.add(grid);
+	//scene.add(grid);
 
-	innerShield = new THREE.Mesh( geometry, innerShieldMaterials );
-	outerShield = new THREE.Mesh( geometry, outerShieldMaterials );
+	innerShield = new THREE.Mesh( geometry, new THREE.MultiMaterial(innerShieldMaterials) );
+	outerShield = new THREE.Mesh( geometry, new THREE.MultiMaterial(outerShieldMaterials) );
 	innerShield.scale.set(.995,.995,.995);
 	innerShield.rotation.set(0,0.78539816,0); //radians, not degrees... get's me every time
 	outerShield.rotation.set(0,0.78539816,0); //radians, not degrees... get's me every time
 	scene.add( innerShield );
 	scene.add( outerShield );
 	camera.position.set(100,10,0);
-	//outerShield.scale.set(.8,.85,1);
+	outerShield.scale.set(1,.7,1);
+	innerShield.scale.set(1,.7,1);
 	var loader = new THREE.TextureLoader();
 
 	loader.load(
@@ -112,6 +112,8 @@ function setShieldStrength(shields){
 	for(var i = 0;i < shields.length;i++){
 		outerShieldMaterials[i].opacity = shields[i] / 2;
 		innerShieldMaterials[i].opacity = shields[i] / 2;
+		outerShieldMaterials[i].color.set(changeHue("#00b7ff",-(260 - (260 * shields[i]))));
+		innerShieldMaterials[i].color.set(changeHue("#00b7ff",-(260 - (260 * shields[i]))));
 	}
 }
 
@@ -124,6 +126,138 @@ var render = function () {
 	render();
 function isOdd(num) { return (num % 2) == 1;}
 
+function changeHue(rgb, degree) {
+    var hsl = rgbToHSL(rgb);
+    hsl.h += degree;
+    if (hsl.h > 360) {
+        hsl.h -= 360;
+    }
+    else if (hsl.h < 0) {
+        hsl.h += 360;
+    }
+    return hslToRGB(hsl);
+}
+
+// exepcts a string and returns an object
+function rgbToHSL(rgb) {
+    // strip the leading # if it's there
+    rgb = rgb.replace(/^\s*#|\s*$/g, '');
+
+    // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+    if(rgb.length == 3){
+        rgb = rgb.replace(/(.)/g, '$1$1');
+    }
+
+    var r = parseInt(rgb.substr(0, 2), 16) / 255,
+        g = parseInt(rgb.substr(2, 2), 16) / 255,
+        b = parseInt(rgb.substr(4, 2), 16) / 255,
+        cMax = Math.max(r, g, b),
+        cMin = Math.min(r, g, b),
+        delta = cMax - cMin,
+        l = (cMax + cMin) / 2,
+        h = 0,
+        s = 0;
+
+    if (delta == 0) {
+        h = 0;
+    }
+    else if (cMax == r) {
+        h = 60 * (((g - b) / delta) % 6);
+    }
+    else if (cMax == g) {
+        h = 60 * (((b - r) / delta) + 2);
+    }
+    else {
+        h = 60 * (((r - g) / delta) + 4);
+    }
+
+    if (delta == 0) {
+        s = 0;
+    }
+    else {
+        s = (delta/(1-Math.abs(2*l - 1)))
+    }
+
+    return {
+        h: h,
+        s: s,
+        l: l
+    }
+}
+
+// expects an object and returns a string
+function hslToRGB(hsl) {
+    var h = hsl.h,
+        s = hsl.s,
+        l = hsl.l,
+        c = (1 - Math.abs(2*l - 1)) * s,
+        x = c * ( 1 - Math.abs((h / 60 ) % 2 - 1 )),
+        m = l - c/ 2,
+        r, g, b;
+
+    if (h < 60) {
+        r = c;
+        g = x;
+        b = 0;
+    }
+    else if (h < 120) {
+        r = x;
+        g = c;
+        b = 0;
+    }
+    else if (h < 180) {
+        r = 0;
+        g = c;
+        b = x;
+    }
+    else if (h < 240) {
+        r = 0;
+        g = x;
+        b = c;
+    }
+    else if (h < 300) {
+        r = x;
+        g = 0;
+        b = c;
+    }
+    else {
+        r = c;
+        g = 0;
+        b = x;
+    }
+
+    r = normalize_rgb_value(r, m);
+    g = normalize_rgb_value(g, m);
+    b = normalize_rgb_value(b, m);
+
+    return rgbToHex(r,g,b);
+}
+
+function normalize_rgb_value(color, m) {
+    color = Math.floor((color + m) * 255);
+    if (color < 0) {
+        color = 0;
+    }
+    return color;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
 /*
 	Not, Three.js crap
 */
+
+//variables
+var shields;
+
+//database observers
+Interstellar.onDatabaseValueChange("shields.strength",function(newData){
+	if(newData == null){
+		Interstellar.setDatabaseValue("shields.strength",{"ventral" : 1,"dorsal" : 1,"forward" : 1,"aft" : 1,"port" : 1,"starboard" : 1});
+		return;
+	}
+	shields = newData;
+	setShieldStrength([shields.dorsal,shields.forward,shields.port,shields.aft,shields.starboard,shields.ventral]);
+});
