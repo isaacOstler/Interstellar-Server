af02a2832d9c4320a7caa76f82ecea1a5f00d6bc
isaacOstler <isaacOstler1999@gmail.com>
2017-08-13 14:05:57 -0600

    Removed the voyager cards folder (stored on my computer for now)

--- Voyager controls/cards/engineering-reactor-stress/script.js
+++
@@ -1,9 +0,0 @@
-Interstellar.onDatabaseValueChange("engineering.reactorStatus.reactorStress",function(newValue){
-	if(newValue == null){
-		setDatabaseValue("engineering.reactorStatus.reactorStress", .1);
-		return;
-	}
-	var boxHeight = $("#stressBarFill").height();
-	var MaskHeight = boxHeight * (1 - newValue);
-	$("#stressBarMask").height(MaskHeight);
-});
