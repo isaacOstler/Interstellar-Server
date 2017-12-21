42422570d3ca9e354b1f9ab103a00ef097461668
isaacOstler <isaacOstler1999@gmail.com>
2017-12-21 03:31:45 -0700

    Greedy best-first search for new path finding, working on A* for next commit (paths are super accurate right now, just a little slow to compute for long travels)

--- cards/Security-Tracking/script.js
+++ cards/Security-Tracking/script.js
@@ -7,7 +7,8 @@ var gridWidth = 30,
 	worldMap = [],
 	cellWidth,
 	cellHeight,
-	devDrawMode = true,
+	devDrawMode = false,
+	devDiagnostics = false,
 	allowDiangle = false;
 
 //type of world tiles 
@@ -31,6 +32,9 @@ initWorld(function(){
 
 function drawPath(startX,startY,endX,endY){
 	var path = getPathForPoints(startX,startY,endX,endY);
+	if(path == null){
+		return;
+	}
 
 	var ctx = canvas[0].getContext('2d');
 	var width = canvas.width(),
@@ -58,14 +62,15 @@ function drawPath(startX,startY,endX,endY){
 
 	for(var i = 0;i < gridWidth;i++){
 		for(var j = 0;j < gridHeight;j++){
-			var g,h;
-			g = Math.abs(i - startX) + Math.abs(j - startY);
-			h = Math.abs(endX - i) + Math.abs(endY - j);
-			var f = diagonalHeuristic(i,j,endX,endY) * diagonalHeuristic(startX,startY,j,i);
+			var g = manhattenHeuristic(startX,startY,i,j),
+				h = manhattenHeuristic(i,j,endX,endY) * 1.5;
+				f = g + h;
+			ctx.beginPath();
+			ctx.fillStyle = Interstellar.rotateHue("#00ff00",(f / 50) * -360);
 			ctx.fillText(Math.round(f),i * cellHeight,j * cellWidth);
+			ctx.stroke();
 		}
 	}
-	ctx.stroke();
 }
 
 function startPathfindingTest(){
@@ -79,7 +84,7 @@ function startPathfindingTest(){
 			x = Math.floor(Math.min(Math.max(event.offsetX / height,0),1) * gridWidth),
 			y = Math.floor(Math.min(Math.max(event.offsetY / width,0),1) * gridHeight);
 
-		drawPath(1,1,x,y);
+		drawPath(10,10,x,y);
 		canvas.on("mousemove.draw",function(event){
 			var width = canvas.width(),
 				height = canvas.height(),
@@ -100,112 +105,212 @@ function startPathfindingTest(){
 }
 
 function getPathForPoints(startY,startX,endY,endX){
-	var openList = [{"x" : startX,"y" : startY}],
-		blacklist = [];
-	do{
-		var x = openList[openList.length - 1].x,
-			y = openList[openList.length - 1].y;
-		//first we need to get all the adjacent squares, and compute their F score
-		var adjacentSquares = [];
-		if(y - 1 >= 0){
-			//top good
-			adjacentSquares.splice(adjacentSquares.length,0,{"x" : x,"y" : y - 1});
-			if(x - 1 >= 0 && allowDiangle){
-				//top left good
-				adjacentSquares.splice(adjacentSquares.length,0,{"x" : x - 1,"y" : y - 1});
-			}
-			if(x + 1 <= gridWidth && allowDiangle){
-				//top right good
-				adjacentSquares.splice(adjacentSquares.length,0,{"x" : x + 1,"y" : y - 1});
-				
-			}
+	var openList = [], //places we have explored
+		closedList = [{"x" : startX,"y" : startY,"breadcrumb" : {"x" : startX, "y" : startY}}], //border
+		detectedPath = false,
+		path = [],
+		lastX = 0,
+		lastY = 0;
+	while(!detectedPath){
+		if(closedList.length == 0){
+			//AAAA!  It's not possible to reach this position!
+			return null;
 		}
-		if(x + 1 <= gridWidth){
-			//right good
-			adjacentSquares.splice(adjacentSquares.length,0,{"x" : x + 1,"y" : y});
-		}
-		if(x - 1 >= 0){
-			//left good
-			adjacentSquares.splice(adjacentSquares.length,0,{"x" : x - 1,"y" : y});
-		}
-		if(y + 1 <= gridHeight){
-			//bottom good
-			adjacentSquares.splice(adjacentSquares.length,0,{"x" : x,"y" : y + 1});
-			if(x + 1 <= gridHeight && allowDiangle){
-				//bottom right good
-				adjacentSquares.splice(adjacentSquares.length,0,{"x" : x + 1,"y" : y + 1});
+		for(var n = 0;closedList.length > 0;n = 0){
+			if(detectedPath){
+				break;
 			}
-			if(x - 1 >= 0 && allowDiangle){
-				//bottom left good
-				adjacentSquares.splice(adjacentSquares.length,0,{"x" : x - 1,"y" : y + 1});
-			}
-		}
 
-		var refinedPathOptions = [];
-		//now we need to remove any that aren't suitable
-		for(var i = 0;i < adjacentSquares.length;i++){
-			if(worldMap.length < adjacentSquares[i].y){
-				//error checking
-				continue;
-			}
-			if(worldMap[adjacentSquares[i].y].length < adjacentSquares[i].x){
-				//error checking
-				continue;
-			}
-			if(worldMap[adjacentSquares[i].y][adjacentSquares[i].x].state != "closed"){
-					//can't walk through walls
-				var detected = false;
-				for(var j = 0;j < openList.length;j++){
-					if(adjacentSquares[i].x == openList[j].x && adjacentSquares[i].y == openList[j].y){
-						//can't backtrack (unless we have no option)
-						if(adjacentSquares.length > 1){
-							detected = true;
+			if(devDiagnostics){
+				var ctx = canvas[0].getContext('2d');
+				var width = canvas.width(),
+					height = canvas.height();
+				
+
+				cellWidth = width / gridWidth,
+				cellHeight = height / gridHeight;
+
+				//clear the canvas
+				ctx.clearRect(0,0,height,width);
+
+				drawCanvas();
+
+				ctx.beginPath();
+
+
+				for(var i = 0;i < closedList.length;i++){
+					ctx.rect(closedList[i].y * cellHeight,closedList[i].x * cellWidth,cellHeight,cellWidth);
+				}
+
+				ctx.fillStyle = "rgba(255,0,0,.5)";
+				ctx.fill();
+				ctx.stroke();
+
+				ctx.beginPath();
+
+
+				for(var i = 0;i < openList.length;i++){
+					ctx.rect(openList[i].y * cellHeight,openList[i].x * cellWidth,cellHeight,cellWidth);
+					var xDirection = openList[i].x - openList[i].breadcrumb.x;
+						yDirection = openList[i].y - openList[i].breadcrumb.y;
+
+					console.log(xDirection,yDirection)
+					ctx.moveTo(openList[i].y * cellHeight + (cellHeight / 2),openList[i].x * cellWidth + (cellWidth / 2));
+					if(xDirection != 0){
+						if(xDirection == -1){
+							//moving left
+							ctx.lineTo(openList[i].y * cellHeight + (cellHeight / 2),openList[i].x * cellWidth);
+						}else{
+							//moving right
+							ctx.lineTo(openList[i].y * cellHeight + (cellHeight / 2),openList[i].x * cellWidth + cellWidth);
+						}
+					}else if(yDirection != 0){
+						if(yDirection == -1){
+							//moving down
+							ctx.lineTo(openList[i].y * cellHeight + cellHeight,openList[i].x * cellWidth + (cellWidth / 2));
+						}else{
+							//moving up
+							ctx.lineTo(openList[i].y * cellHeight,openList[i].x * cellWidth + (cellWidth / 2));
 						}
 					}
 				}
-				for(var j = 0;j < blacklist.length;j++){
-					if(adjacentSquares[i].x == blacklist[j].x && adjacentSquares[i].y == blacklist[j].y){
-						detected = true;
+
+				ctx.fillStyle = "rgba(0,255,0,.5)";
+				ctx.fill();
+				ctx.stroke();
+			}
+
+
+			var x = closedList[n].x,
+			y = closedList[n].y,
+			breadcrumb = {"x" : x,"y" : y};
+			var adjacentSquares = [];
+
+			if(devDiagnostics){
+				ctx.beginPath();
+
+
+				for(var i = 0;i < openList.length;i++){
+					ctx.rect(y * cellHeight,x * cellWidth,cellHeight,cellWidth);
+				}
+
+				ctx.fillStyle = "rgba(255,255,255,.5)";
+				ctx.fill();
+				ctx.stroke();
+			}
+
+			if(y - 1 > 0){
+				//top good
+				adjacentSquares.splice(adjacentSquares.length,0,{"x" : x,"y" : y - 1});
+				if(x - 1 >=0 && allowDiangle){
+					//top left good
+					adjacentSquares.splice(adjacentSquares.length,0,{"x" : x - 1,"y" : y - 1});
+				}
+				if(x + 1 < gridWidth && allowDiangle){
+					//top right good
+					adjacentSquares.splice(adjacentSquares.length,0,{"x" : x + 1,"y" : y - 1});
+					
+				}
+			}
+			if(x + 1 < gridWidth){
+				//right good
+				adjacentSquares.splice(adjacentSquares.length,0,{"x" : x + 1,"y" : y});
+			}
+			if(x - 1 > 0){
+				//left good
+				adjacentSquares.splice(adjacentSquares.length,0,{"x" : x - 1,"y" : y});
+			}
+			if(y + 1 < gridHeight){
+				//bottom good
+				adjacentSquares.splice(adjacentSquares.length,0,{"x" : x,"y" : y + 1});
+				if(x + 1 <= gridHeight && allowDiangle){
+					//bottom right good
+					adjacentSquares.splice(adjacentSquares.length,0,{"x" : x + 1,"y" : y + 1});
+				}
+				if(x - 1 >= 0 && allowDiangle){
+					//bottom left good
+					adjacentSquares.splice(adjacentSquares.length,0,{"x" : x - 1,"y" : y + 1});
+				}
+			}
+
+			var refinedPathOptions = [];
+			//now we need to remove any that aren't suitable
+			for(var i = 0;i < adjacentSquares.length;i++){
+				if(worldMap[adjacentSquares[i].y][adjacentSquares[i].x].state != "closed"){
+					var wasDetected = false;
+					for(var j = 0;j < openList.length;j++){
+						if(adjacentSquares[i].x == openList[j].x && adjacentSquares[i].y == openList[j].y){
+							wasDetected = true;
+						}
+					}
+					for(var j = 0;j < closedList.length;j++){
+						if(adjacentSquares[i].x == closedList[j].x && adjacentSquares[i].y == closedList[j].y){
+							wasDetected = true;
+						}
+					}
+					if(!wasDetected){
+						refinedPathOptions.splice(refinedPathOptions.length,0,{"x" : adjacentSquares[i].x,"y" : adjacentSquares[i].y,"breadcrumb" : breadcrumb});
 					}
 				}
-				if(!detected){
-					refinedPathOptions.splice(refinedPathOptions.length,0,{"x" :adjacentSquares[i].x,"y" : adjacentSquares[i].y});
+			}
+			adjacentSquares = refinedPathOptions;
+			//now we need to compute the f and g and h score for each one
+			//add to openList
+			openList.splice(openList.length,0,closedList[n]);
+			//remove this from the border (closed list)
+			closedList.splice(n,1);
+			//add the new borders
+			for(var i = 0;i < adjacentSquares.length;i++){
+				closedList.splice(closedList.length,0,adjacentSquares[i])
+			}
+			for(var i = 0;i < openList.length;i++){
+				if(openList[i].x == endX && openList[i].y == endY){
+					//WE DID IT!!!!!!!!!  YESSSS
+					detectedPath = true;
+					path[0] = {"x" : lastX, "y" : lastY};
+					lastX = openList[i].x;
+					lastY = openList[i].y;
+					closedList = [];
 				}
 			}
 		}
-		adjacentSquares = refinedPathOptions;
-		//now we need to compute the f and g and h score for each one
-		//f = g + h
-		//g = cost from start
-		//h = estimated cost from end
-		var lowestFScore = undefined,
-			nextSquare = {"x" : -1, "y" : -1};
-		for(var i = 0;i < adjacentSquares.length;i++){
-			var f = diagonalHeuristic(adjacentSquares[i].x,adjacentSquares[i].y,endX,endY);// + diagonalHeuristic(startX,startY,adjacentSquares[i].y,adjacentSquares[i].x);
-			//g = Math.abs(adjacentSquares[i].x - openList[0].x) + Math.abs(adjacentSquares[i].y - openList[0].y);
-			//h = Math.abs(adjacentSquares[i].x - endX) + Math.abs(adjacentSquares[i].y - endY);
+	}
+	while(path[0].x != startX || path[0].y != startY){
+		//clear the canvas
+		if(devDiagnostics){
+			ctx.clearRect(0,0,height,width);
 
-			if(lowestFScore == undefined || f < lowestFScore){
-				lowestFScore = f;
-				nextSquare = {"x" : adjacentSquares[i].x,"y" : adjacentSquares[i].y}
-			}else if(lowestFScore == f){
-				//tie!
-				lowestFScore = f;
-				nextSquare = {"x" : adjacentSquares[i].x,"y" : adjacentSquares[i].y}
+			drawCanvas();
+
+			ctx.beginPath();
+
+
+			for(var i = 0;i < path.length;i++){
+				ctx.rect(path[i].y * cellHeight,path[i].x * cellWidth,cellHeight,cellWidth);
+			}
+
+			ctx.fillStyle = "rgba(255,0,0,.5)";
+			ctx.fill();
+			ctx.stroke();
+		}
+
+		for(var i = openList.length;i > 0;i--){
+			//work backwards through breadcrumbs
+			if(openList[i - 1].x == lastX && openList[i - 1].y == lastY){
+				path.unshift({"x" : openList[i - 1].x,"y" : openList[i - 1].y});
+				lastX = openList[i - 1].breadcrumb.x;
+				lastY = openList[i - 1].breadcrumb.y;
+				break;
 			}
 		}
-		if(nextSquare.x != -1 && nextSquare.y != -1){
-			openList.splice(openList.length,0,nextSquare);
-		}else{
-			//we are stuck, backtrack, and blacklist this block
-			if(openList.length > 0){
-				//blacklist.splice(blacklist.length,0,{"x" : openList[openList.length - 1].x,"y" : openList[openList.length - 1].y});
-				openList.splice(openList.length,1);
-			}
-		}
-	}while((openList[openList.length - 1].x != endX || openList[openList.length - 1].y != endY));
-	return openList;
+	}
+	return path;
 }
+
+function manhattenHeuristic(startX,startY,endX,endY){
+	return Math.abs(startX - endX) + Math.abs(startY - endY);
+}
+
 function diagonalHeuristic(startX,startY,endX,endY){
 	var D = 1;
 	var D2 = Math.sqrt(2); //cost of moving diagonally
