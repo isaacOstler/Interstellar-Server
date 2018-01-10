2b7a3df38ceb74965dbec4be32302af771dee517
isaacOstler <isaacOstler1999@gmail.com>
2018-01-10 06:51:53 -0700

    Onscreen keyboard finished

--- cards/onscreen-keyboard/script.js
+++ cards/onscreen-keyboard/script.js
@@ -1,8 +1,13 @@
 var onScreenKeyboardClass = function(){
+    var keyboard = $("#onscreen-keyboard");
+
     var lastKnownElement = undefined,
-        shiftActive = false;
+        keyboardOut = false,
+        shiftActive = false,
+        capsLockActive = false;
 
     recalcEventListeners();
+    drawShiftGUI(false);
 
     $('body').on('DOMNodeInserted', function () {
         recalcEventListeners();
@@ -14,20 +19,142 @@ var onScreenKeyboardClass = function(){
         $("input[type=text]").click(function(event){
             lastKnownElement = event.target;
         });
+        $("#showOnscreen-keyboardButton").off();
+        $("#showOnscreen-keyboardButton").click(function(event){
+            Interstellar.playRandomBeep();
+            if(!keyboardOut){
+                openKeyboard();
+            }else{
+                hideKeyboard();
+            }
+        });
         $(".onscreen-keyboard_key").click(function(event){
             Interstellar.playRandomBeep();
-            var key = $(event.target).attr("key");
-            if(key == undefined){
-              key = $(event.target).html();
+            var key = $(event.target).html();
+            if(key == "SHIFT"){
+                shiftActive = !shiftActive;
+                drawShiftGUI(shiftActive);
+                return;
             }
-            if(shiftActive){
-              key.toUpperCase();
+            if(key == "TAB"){
+                insertAtCaret(lastKnownElement,"    ");
+                return;
+            }
+            if(key == "SPACE"){
+                insertAtCaret(lastKnownElement," ");
+                return;
+            }
+            if(key == "CMD" || key == "ALT" || key == "FN" || key == "CTRL"){
+                return;
+            }
+            if(key == "⌫ DELETE"){
+                deleteCharacter(lastKnownElement);
+                return;
+            }
+            if(key == "ENTER"){
+                insertAtCaret(lastKnownElement,"\n");
+                return;
+            }
+            if(key == "HIDE"){
+                hideKeyboard();
+                return;
+            }
+            if(key == "CAPS LOCK"){
+                capsLockActive = !capsLockActive;
+                drawCrapsGUI(capsLockActive);
+                return;
+            }
+            if(key == undefined){
+                key = $(event.target).html();
+                if(shiftActive){
+                  key = key.toUpperCase();
+                }else{
+                  key = key.toLowerCase();
+                }
             }else{
-              key.toLowerCase();
+                if(shiftActive){
+                    key = $(event.target).attr("shiftKey");
+                }
             }
             insertAtCaret(lastKnownElement,key);
         });
+        $("#onscreen-keyboard_topMoveDetector").off();
+        $("#onscreen-keyboard_topMoveDetector").on("mousedown.keyboardDrag",function(event){
+            let initalOffsetX = event.offsetX;
+                initalOffsetY = event.offsetY,
+                updateFunction = function(passedEvent){
+                var calculatedX = passedEvent.pageX - initalOffsetX;
+                var calculatedY = passedEvent.pageY - initalOffsetY;
+                keyboard.css("left",calculatedX + "px");
+                keyboard.css("top",calculatedY + "px");
+            }
+            updateFunction(event);
+            $(document.body).on("mousemove.keyboardDrag",function(event){
+                updateFunction(event);
+            });
+            $(document.body).on("mouseup.keyboardDrag",function(event){
+                updateFunction(event);
+                $(document.body).off("mousemove.keyboardDrag");
+                $(document.body).off("mouseup.keyboardDrag");
+            });
+        });
+    }
 
+    function drawCrapsGUI(state){
+        shiftActive = false;
+        drawShiftGUI(shiftActive);
+        if(state){
+            $(".onscreen-keyboard_key").each(function() {
+                if($(this).attr("keyType") == "alpha" && $(this).attr("keyType") != "hide"){
+                    $(this).html($(this).attr("shiftKey"));
+                }
+            });
+            $("#onscreen-keyboard_alphaKeys_caps").css("background-color","rgba(255,255,255,.7)");
+        }else{
+            $(".onscreen-keyboard_key").each(function() {
+                if($(this).attr("keyType") == "alpha" && $(this).attr("keyType") != "hide"){
+                    $(this).html($(this).attr("key"));
+                }
+            });
+            $("#onscreen-keyboard_alphaKeys_caps").css("background-color","");
+        }
+    }
+
+    function drawShiftGUI(state){
+        if(state){
+            $(".onscreen-keyboard_key").each(function() {
+                if($(this).attr("keyType") != "hide"){
+                    $(this).html($(this).attr("shiftKey"));
+                }
+            });
+            $("#onscreen-keyboard_alphaKeys_leftShift").css("background-color","rgba(255,255,255,.7)");
+            $("#onscreen-keyboard_alphaKeys_rightShift").css("background-color","rgba(255,255,255,.7)");
+        }else{
+            $(".onscreen-keyboard_key").each(function() {
+                if(capsLockActive && $(this).attr("keyType") == "alpha")
+                {
+                    if($(this).attr("keyType") != "hide"){
+                        $(this).html($(this).attr("shiftKey"));
+                    }
+                }else{
+                    if($(this).attr("keyType") != "hide"){
+                        $(this).html($(this).attr("key"));
+                    }
+                }
+            });
+            $("#onscreen-keyboard_alphaKeys_leftShift").css("background-color","");
+            $("#onscreen-keyboard_alphaKeys_rightShift").css("background-color","");
+        }
+    }
+
+    function deleteCharacter(txtarea){
+        if(txtarea == undefined){
+            return;
+        }
+        var value = $(txtarea).val();
+        if(value.length > 0){
+            $(txtarea).val(value.substring(0,value.length - 1));
+        }
     }
 
     function insertAtCaret(txtarea, text) {
@@ -64,6 +191,16 @@ var onScreenKeyboardClass = function(){
         }
 
         txtarea.scrollTop = scrollPos;
+    }
+    function hideKeyboard(){
+        keyboardOut = false;
+        keyboard.stop();
+        keyboard.animate({"left" : -1215});
+    }
+    function openKeyboard(){
+        keyboardOut = true;
+        keyboard.stop();
+        keyboard.animate({"left" : 200});
     }
 }
 
