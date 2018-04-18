8132cc6c1eb7030b0ec2ebf90f334b613d60af01
isaacOstler <isaacOstler1999@gmail.com>
2018-04-17 20:58:20 -0600

    Started on the weapons control core widget

---
+++ cards/weapons-control-core/script.js
@@ -0,0 +1,22 @@
+Interstellar.addCoreWidget("Weapons Control",function(){
+	var thisWidget = this;
+
+	//variables
+	
+	//DOM References
+	
+	//init calls
+
+	//interstellar calls
+	thisWidget.onResize = function(){
+		drawGUI();
+	}
+
+	//functions
+
+	//preset observers
+
+	//database observers
+
+	//event handlers
+});
