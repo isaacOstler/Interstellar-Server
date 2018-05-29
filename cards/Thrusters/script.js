18307834e1b7993e169bc64572402f7065e698df
isaacOstler <isaacOstler1999@gmail.com>
2018-05-29 11:17:22 -0600

    Started the Thrusters screen.  Created the thrusters class

---
+++ cards/Thrusters/script.js
@@ -0,0 +1,19 @@
+//DOM Refrences
+var canvas = $("#canvas");
+
+//variables
+
+//init calls
+
+//class instances
+var thrusterDisplay = new ThrustersDisplayClass(canvas);
+
+//preset obeservers
+
+//database observers
+
+//functions
+
+//event listeners
+
+//intervals
