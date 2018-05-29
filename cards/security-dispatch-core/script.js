b4c0406a6387c781b8e9d8fc5ca91fe9064761a3
isaacOstler <isaacOstler1999@gmail.com>
2018-05-29 00:09:41 -0600

    Started work on the security dispatch core widget (GUI 95% finished)

---
+++ cards/security-dispatch-core/script.js
@@ -0,0 +1,27 @@
+Interstellar.addCoreWidget("Security Dispatch",function(){
+	var thisWidget = this;
+
+	//variables
+	
+	//DOM References
+	
+	//init calls
+	
+	//interstellar calls
+	thisWidget.onResize = function(){
+		drawGUI();
+	}
+
+	//functions
+
+	function drawGUI(){
+		
+	}
+
+	//preset observers
+
+	//database observers
+	
+	//event handlers
+	
+});
