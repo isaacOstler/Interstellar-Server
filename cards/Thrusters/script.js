1845c6628c8cc2637d90d012f233dab84736cf6d
isaacOstler <isaacOstler1999@gmail.com>
2018-05-31 02:21:15 -0600

    Thrusters no longer operate when damaged or with no power

--- cards/Thrusters/script.js
+++ cards/Thrusters/script.js
@@ -1,6 +1,9 @@
 //DOM Refrences
 var directionalCanvas = $("#canvas"),
-    verticalCanvas = $("#verticalCanvas");
+    verticalCanvas = $("#verticalCanvas"),
+    systemOfflineElement = $("#systemOffline"),
+    systemOfflineElement_header = $("#systemOffline_header"),
+    systemOfflineElement_subtitle = $("#systemOffline_subtitle");
 
 //variables
 var thrusterPower = 0,
@@ -18,6 +21,26 @@ var verticalDisplay = new VerticalThrustersClass(verticalCanvas);
 //preset obeservers
 
 //database observers
+Interstellar.onDatabaseValueChange("ship.systems",function(newData){
+    if(newData == null){
+        return; //DO NOT SET THIS VALUE HERE!
+    }
+    for(var i = 0;i < newData.length;i++){
+        if(newData[i].systemName.toLowerCase() == "thrusters"){
+            if(newData[i].isDamaged){
+                systemOfflineElement.fadeIn();
+                systemOfflineElement_header.html("SYSTEM OFFLINE");
+                systemOfflineElement_subtitle.html("THIS SYSTEM HAS BEEN DAMAGED AND IS UNRESPONSIVE");
+            }else if(newData[i].requiredPower[0] > newData[i].systemPower){
+                systemOfflineElement.fadeIn();
+                systemOfflineElement_header.html("INSUFFICIENT POWER");
+                systemOfflineElement_subtitle.html("THIS SYSTEM HAS INSUFFICIENT POWER TO OPERATE");
+            }else{
+                systemOfflineElement.fadeOut();
+            }
+        }
+    }
+});
 
 //functions
 
